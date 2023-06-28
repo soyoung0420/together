b0bd7af82a866a9bd17f82fa0b1a3798963e263a
soyoung0420 <my5126@naver.com>
2023-06-28 12:41:22 +0900

    Soyoung0420Servlet 수정!

--- src/main/java/com/together/Soyoung0420Servelet2.java
+++ src/main/java/com/together/Soyoung0420Servelet2.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class Soyoung0420Servelet2
  */
-@WebServlet("/soyoung0420")
+@WebServlet("/soyoung0420/new")
 public class Soyoung0420Servelet2 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
