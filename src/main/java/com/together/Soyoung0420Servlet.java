261159678497c6f82d735672543e951215bda3f7
soyoung0420 <my5126@naver.com>
2023-06-28 11:40:33 +0900

    Soyoung0420Servlet 추가!

---
+++ src/main/java/com/together/Soyoung0420Servlet.java
@@ -0,0 +1,25 @@
+package com.together;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Soyoung0420Servlet
+ */
+@WebServlet("/soyoung0420") // url 주소는 자신의 아이디로 설정할 것! 
+public class Soyoung0420Servlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append("Hello, soyoung0420");
+	}
+
+}
